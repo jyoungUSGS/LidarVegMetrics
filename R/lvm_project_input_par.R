9824477102dba5dbed6e8aedbd3ab02536cc8a5a
nfkruska <nfkruska@ncsu.edu>
2018-01-26 13:05:57 -0500

    flow update
    reorganize order of operations - increased processing speed.

--- R/lvm_project_input_par.R
+++ R/lvm_project_input_par.R
@@ -4,34 +4,35 @@ library(tools)
 library(parallel)
 library(rgdal)
 
-# Global dbh in meters
-# must be changed if input unit is not meters
-dbh <<- 1.37
-
 # Absolute path to working directory for output files
-project_dir <- "C:/Users/nfkruska/Documents/data/SHEN/va_shen_2015"
+project_dir <- "C:/Users/nfkruska/Documents/projects/SHEN/va_shen_2011"
 
 # Relative or absolute path to location of lidar files
 lidar_dir <- "./HAG"
+# output folder sub directory to project directory
+out_dir <- "./tiles"
+# output raster resolution(s). Can be integer or list of integers
+# will create folder for each resolution in out_dir
+output_res <- c(10, 25)
 
 # Input CRS of lidar files found by EPSG code
 # NAD83 / UTM zone 17N: 26917
 # NAD83(2011) / Conus Albers: 6350
 epsg_code <- 26917
 
-# points under this value will not be considered as vegetation
-veg_floor <- dbh
-# points above this value will not be considered as vegetation
+# points above/below values will not be considered as vegetation
+# dbh: 1.37
+veg_floor <- 1.37
 veg_ceiling <- 50
-# output raster resolution(s). Can be integer or list of integers
-output_res <- c(10, 25)
+
 # boolean: have the input points been normalized?
-# NOT CURRENTLY SUPPORTED
-# POINTS MUST ALREADY BE NORMALIZED
+# !!~NOT CURRENTLY SUPPORTED~!!
+# !!~POINTS MUST ALREADY BE NORMALIZED~!!
 hag_nrml <- T
+
 # raster format for output
-# "HFA": .img
-# "GTiff": .tif
+# .img: "HFA"
+# .tif: "GTiff"
 ras_fmt <- "GTiff"
 
 # =============================================================================
@@ -50,86 +51,25 @@ if (length(input_crs) == 0){
   stop("EPSG code was not found.")
 }
 
-folders <- c("./tiles", "./mosaics")
-names(folders) <- c("tiles", "mosaics")
-dir.create(folders["tiles"])
-# dir.create(folders["mosaics"])
-# =============================================================================
+dir.create(out_dir)
 
-calc_metrics <- function(x, CRS, output_dir, resolution, nrml = T){
-  
-  format_tile_name <- function(x){
-    tn <- basename(tools::file_path_sans_ext(x))
-    np <- strsplit(tn, "_")
-    tn <- paste0(unlist(np[[1]][1:5]), collapse = "_")
-    tn <- paste(tn, resolution, sep = "_")
-    return(tn)
-  }
-  tile_name <- format_tile_name(x)
-  print(tile_name)
-  
-  save_output <- function(output_dir, folder, tile_name, product, file){
-    output_path <- file.path(output_dir, folder, paste(tile_name, product,
-    sep = "_"))
-    if (file.exists(output_path)){
-      warning(paste0("File ", output_path,
-      " already exists. It will be overwritten."))
-    }
-    
-    if (product %in% c("hden", "hcnt", "hpct")){
-      raster::writeRaster(file, output_path, ras_fmt, overwrite = T,
-      options = "COMPRESS = DEFLATE")
-    } else {
-      raster::writeRaster(file, output_path, ras_fmt, overwrite = T,
-      bylayer = T, suffix = names(file), options = "COMPRESS = DEFLATE")
-    }
+# =============================================================================
+save_output <- function(output_dir, folder, tile_name, product, file){
+  output_path <- file.path(output_dir, folder, paste(tile_name, product,
+                                                     sep = "_"))
+  if (file.exists(output_path)){
+    warning(paste0("File ", output_path,
+                   " already exists. It will be overwritten."))
   }
   
-  # data load with CRS
-  las_data <- USGSlvm::readLidarData(x, CRS)
-  # at this time, expecting normalized points
-  names(las_data)[1] <- "Z_agl"
-  
-  # if data does not fall within range, class as noise
-  las_data$Classification[las_data$Classification == 1 & veg_floor >
-                          las_data$Z_agl | las_data$Z_agl > veg_ceiling] <- 7
-  
-  # classify the points by height above ground
-  las_data <- USGSlvm::classifyByHeight(las_data)
-  
-  # metrics functions
-  stats <- USGSlvm::calcPointStatistics(las_data, resolution)
-  save_output(output_dir, "stats", tile_name, "stat", stats)
-  rm(stats)
-  
-  vdr <- USGSlvm::calcVertDistRatio(las_data, resolution)
-  save_output(output_dir, "ratios", tile_name, "vdrs", vdr)
-  rm(vdr)
-  
-  create_canopy_stack <- function(x, res){
-    ccov <- USGSlvm::calcCanopyCover(x, res)
-    cdens <- USGSlvm::calcCanopyDensity(x, res)
-    s <- raster::stack(ccov, cdens)
-    names(s) <- c("ccov", "cden")
-    return(s)
+  if (product %in% c("hden", "hcnt", "hpct")){
+    raster::writeRaster(file, output_path, ras_fmt, overwrite = T,
+                        options = "COMPRESS = DEFLATE")
+  } else {
+    raster::writeRaster(file, output_path, ras_fmt, overwrite = T,
+                        bylayer = T, suffix = names(file),
+                        options = "COMPRESS = DEFLATE")
   }
-  cnpy <- create_canopy_stack(las_data, resolution)
-  save_output(output_dir, "canopy", tile_name, "cnpy", cnpy)
-  rm(cnpy)
-   
-  hpct <- USGSlvm::calcHeightPercentiles(las_data, resolution)
-  save_output(output_dir, "height_pct", tile_name, "hpct", hpct)
-  rm(hpct)
-  
-  hcnt <- USGSlvm::calcHeightPointCounts(las_data, resolution)
-  save_output(output_dir, "height_cnt", tile_name, "hcnt", hcnt)
-  
-  hdens <- USGSlvm::calcHeightPointPercents(hcnt, resolution)
-  save_output(output_dir, "height_den", tile_name, "hden", hdens)
-  rm(hcnt, hdens, las_data)
-  gc()
-  
-  return(TRUE)
 }
 
 create_res_output <- function(f){
@@ -144,20 +84,75 @@ create_res_output <- function(f){
   return(p)
 }
 
+format_tile_name <- function(x, resolution){
+  tn <- basename(tools::file_path_sans_ext(x))
+  np <- strsplit(tn, "_")
+  tn <- paste0(unlist(np[[1]][1:5]), collapse = "_")
+  tn <- paste(tn, resolution, sep = "_")
+  return(tn)
+}
+
+calc_metrics <- function(x, CRS, output_dir, resolution, nrml = T){
+  # data load with CRS
+  las_data <- USGSlvm::readLidarData(x, CRS)
+  # at this time, expecting normalized points
+  names(las_data)[1] <- "Z_agl"
+  
+  # if data does not fall within range, class as noise
+  las_data$Classification[las_data$Classification == 1 & veg_floor > 
+                            las_data$Z_agl | las_data$Z_agl > veg_ceiling] <- 7
+  
+  # classify the points by height above ground
+  las_data <- USGSlvm::classifyByHeight(las_data)
+  
+  for (res in resolution){
+    res_output <- file.path(output_dir, paste(res, "m", sep = ""))
+    products <- create_res_output(res_output)
+
+    tile_name <- format_tile_name(x, res)
+    print(tile_name)
+  
+    # metrics functions
+    stats <- USGSlvm::calcPointStatistics(las_data, resolution)
+    save_output(res_output, "stats", tile_name, "stat", stats)
+
+
+    vdr <- USGSlvm::calcVertDistRatio(las_data, resolution)
+    save_output(res_output, "ratios", tile_name, "vdrs", vdr)
+
+    create_canopy_stack <- function(x, res){
+      ccov <- USGSlvm::calcCanopyCover(x, res)
+      cdens <- USGSlvm::calcCanopyDensity(x, res)
+      s <- raster::stack(ccov, cdens)
+      names(s) <- c("ccov", "cden")
+      return(s)
+    }
+    cnpy <- create_canopy_stack(las_data, resolution)
+    save_output(res_output, "canopy", tile_name, "cnpy", cnpy)
+
+    hpct <- USGSlvm::calcHeightPercentiles(las_data, resolution)
+    save_output(res_output, "height_pct", tile_name, "hpct", hpct)
+
+    hcnt <- USGSlvm::calcHeightPointCounts(las_data, resolution)
+    save_output(res_output, "height_cnt", tile_name, "hcnt", hcnt)
+
+    hdens <- USGSlvm::calcHeightPointPercents(hcnt, resolution)
+    save_output(res_output, "height_den", tile_name, "hden", hdens)
+  }
+  return(TRUE)
+}
+
 cl <- makeCluster(detectCores() - 1)
 clusterEvalQ(cl, {
   library(rlas); library(sp); library(raster);
   library(rgdal); library(data.table); library(gstat);
   library(USGSlvm); library(tools); library(parallel)
 })
-clusterExport(cl, c("veg_floor", "veg_ceiling", "ras_fmt", "folders"))
+clusterExport(cl, c("veg_floor", "veg_ceiling", "ras_fmt", "create_res_output",
+                    "format_tile_name", "save_output"))
 
-for (res in output_res){
-  res_output <- file.path(folders["tiles"], paste(res, "m", sep = ""))
-  products <- create_res_output(res_output)
+tile_time <- system.time(tiles <- parLapply(cl, lidar_files,
+  calc_metrics, CRS = input_crs, output_dir = out_dir,
+  resolution = output_res))
 
-  tile_time <- system.time(tiles <- parLapply(cl, lidar_files,
-    calc_metrics, CRS = input_crs, output_dir = res_output, resolution = res))
-  gc()
-}
 stopCluster(cl)
